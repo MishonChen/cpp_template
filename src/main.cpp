ee00466960594f3f2e6f9aa1c4c5e99b9114023f
MishonChen <cyke0315@gamil.com>
2024-08-27 13:10:10 +0800

    fect:fmt

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
 #include <fmt/core.h>
-#include <iostream>
 
 auto main() -> int {
   fmt::println("Hello Vcpkg!");
-  fmt::print("Hello World!\n");
-  std::cout << "Hello World!\n";
-  fmt::println("aaa");
   return 0;
 }
